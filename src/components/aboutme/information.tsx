657ca7bd0d3a5f57d8d8fc71f8cf56c2cda2e70d
yuriy.evseev <yuriy.evseev@glasno.tech>
2024-11-16 01:35:38 +0300

    fix text size

--- src/components/aboutme/information.tsx
+++ src/components/aboutme/information.tsx
@@ -9,7 +9,7 @@ export default function Information() {
   return <div className='h-full px-2 w-full mb-12'>
     <div className="size-full  items-center justify-center overflow-hidden pt-8">
       <BoxReveal boxColor={"#5046e6"} duration={0.5}>
-        <p className="text-[3.5rem] font-semibold">
+        <p className="text-[45px] lg:text-[3.5rem] font-semibold">
           <span className="text-[#5046e6]">Евсеев</span> Юрий Владимирович
         </p>
       </BoxReveal>
@@ -21,7 +21,7 @@ export default function Information() {
       </BoxReveal>
       
       <div className="mt-6 duration-500 animate-fade-in">
-        <TypingAnimation className='text-lg text-left' duration={50} text='Меня зовут Юрий Евсеев, я увлеченный фронтенд-разработчик, который любит работать с современными
+        <TypingAnimation className='text-lg text-left' duration={35} text='Меня зовут Юрий Евсеев, я увлеченный фронтенд-разработчик, который любит работать с современными
             технологиями и создавать интуитивно понятные интерфейсы. Мне нравится, когда код не только работает,
             но и выглядит красиво, и особенно, когда он улучшает жизнь пользователей. Я всегда стремлюсь к тому,
             чтобы учиться новому, и получаю настоящее удовольствие от поиска решений сложных задач. В своей
